21f94530179cf619d797a0be4e0220395698a00f
VladTomici14 <vladtomici@gmail.com>
2025-05-04 02:51:25 +0300

    changed the color profile

--- src/pi.py
+++ src/pi.py
@@ -5,7 +5,7 @@ class PiCamera2Stream:
     def __init__(self, resolution=(640, 480)):
         self.picam2 = Picamera2()
         self.picam2.preview_configuration.main.size = resolution
-        self.picam2.preview_configuration.main.format = "BGR888"  # OpenCV-friendly
+        self.picam2.preview_configuration.main.format = "RGB888"  # OpenCV-friendly
         self.picam2.configure("preview")
         self.picam2.start()
         print(f"Camera started with resolution {resolution}")
